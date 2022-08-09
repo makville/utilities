5068b11732367fee90809989bdecd9433399b175
Ayo Makanjuola <makville@gmail.com>
2022-08-09 21:22:56 +0100

    Start of week done

--- src/DateTime.php
+++ src/DateTime.php
@@ -3,10 +3,19 @@ declare(strict_types=1);
 
 namespace Makville\Utilities;
 
+use Cake\Chronos\Chronos;
+
 class DateTime {
 	
+	/**
+     * getStartDateOfCurrentWeek method
+     * Monday is the first day, Sunday is the last day. That is my assumption deal with it :D
+     *
+     * @return \Cake\Chronos\Traits\FactoryTrait
+     */	
 	public static function getStartDateOfCurrentWeek() {
-		return '3333';
+		$now = Chronos::now();
+		return $now->startOfWeek();
 	}
 }
 ?>
